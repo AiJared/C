393874fc922cd4d7c02eab831255000d4db7e3e9
Kerriea-star <ojared511@gmail.com>
2024-08-26 18:29:47 +0300

    variables

---
+++ variables.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    // variable = Allocated space in memory to store a value.
+    //            We refer to a variable's name to access the stored value
+    //            That variable now behaves as if it was the value it contains.
+    //            But we need to declare what type of data we are storing
+    int x; // declaration
+    x = 123; //intialization
+    int y = 321; //declaration + initialization
+
+    int age = 21; //integer
+    float gpa = 2.05; //floating point number
+    char grade = 'C'; //single character
+    char name[] = "Bro"; //array of characters 
+    printf("Hello %s\n", name);
+    printf("You are %d years old.", age);
+    printf("");
+
+}
