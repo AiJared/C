2f24974dc687719ba972cd0542e15cefd730eda0
Kerriea-star <ojared511@gmail.com>
2024-08-26 17:47:21 +0300

    commenting in C

--- intro.c
+++ intro.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 int main() {
+
+    // This is a single line comment
+    /*
+    This 
+    is
+    a
+    multiline
+    comment
+    */
     printf("I like pizza.\n");
     printf("It's really good.\n");
     return 0;
