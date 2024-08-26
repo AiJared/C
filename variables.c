c264239a6d1a56236d04fdd77912a64b120f13ba
Kerriea-star <ojared511@gmail.com>
2024-08-26 18:32:39 +0300

    variables

--- variables.c
+++ variables.c
@@ -14,7 +14,8 @@ int main(){
     char grade = 'C'; //single character
     char name[] = "Bro"; //array of characters 
     printf("Hello %s\n", name);
-    printf("You are %d years old.", age);
-    printf("");
+    printf("You are %d years old.\n", age);
+    printf("Your average grade is %c\n", grade);
+    printf("Your gpa is %f\n", gpa);
 
 }
