eb1ac180f1263ba82cb67af13d2540ab2e9d4640
Kerriea-star <ojared511@gmail.com>
2024-08-26 18:37:02 +0300

    data types

---
+++ datatypes.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+
+    char a = 'C';        // single character %c
+    char b[] = "Bro";     // array of characters %s
+    float c = 3.141592;   // 4bytes (32 bits of precision) 6 - 7 digitd %f
+    
+}
