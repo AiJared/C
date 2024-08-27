58b82dd062d8289629933176c3de3dd0414ef066
Kerriea-star <ojared511@gmail.com>
2024-08-27 06:48:58 +0300

    format specifiers

--- format_specifier.c
+++ format_specifier.c
@@ -11,16 +11,16 @@ int main(){
     // %d = integer
 
     // %.1 = decimal precision
-    // %1 = minimum field width
-    // %- = left margin
+    // %1 = minimum field width (but it will be right aligned)
+    // %- = left align
 
     float item1= 5.75;
     float item2 = 10.00;
     float item3 = 100.99;
 
-    printf("Item 1: $%.2f\n", item1);
-    printf("Item 1: $%.2f\n", item2);
-    printf("Item 1: $%.2f\n", item3);
+    printf("Item 1: $%8.2f\n", item1);
+    printf("Item 1: $%8.2f\n", item2);
+    printf("Item 1: $%8.2f\n", item3);
 
     return 0;
 }
