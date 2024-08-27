ffaf8c4409c81c9439ffb35744e790283b601299
Kerriea-star <ojared511@gmail.com>
2024-08-27 06:50:21 +0300

    format specifiers

--- format_specifier.c
+++ format_specifier.c
@@ -18,9 +18,9 @@ int main(){
     float item2 = 10.00;
     float item3 = 100.99;
 
-    printf("Item 1: $%8.2f\n", item1);
-    printf("Item 1: $%8.2f\n", item2);
-    printf("Item 1: $%8.2f\n", item3);
+    printf("Item 1: $%-8.2f\n", item1);
+    printf("Item 1: $%-8.2f\n", item2);
+    printf("Item 1: $%-8.2f\n", item3);
 
     return 0;
 }
