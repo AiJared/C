767044ddc23394c368171603ed4ce57d8e281d2f
Kerriea-star <ojared511@gmail.com>
2024-08-27 06:31:47 +0300

    data types

--- datatypes.c
+++ datatypes.c
@@ -12,8 +12,8 @@ int main(){
     char f = 120; // 1 byte (-128 to + 127) %d or %c
     unsigned char g = 255; // 1 byte (0 to 255) %d or %c
 
-    short int h = 32767; // 2 bytes (-32,768 to +32,767) %d
-    unsigned short int i = 65535; // 2 bytes (0 to +65,535) %d
+    short h = 32767; // short int 2 bytes (-32,768 to +32,767) %d
+    unsigned short i = 65535; // unsigned short int 2 bytes (0 to +65,535) %d
 
     int j = 2147483647; // 4 bytes (-2,147,483,648 to +2,147,483,647) %d
     unsigned int k = 4294967295L; // 4 bytes (0, to +4,294,967,295) %u
