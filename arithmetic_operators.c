f1c31d649ae7de2efdf71f30af19bac9f1dd8c0f
Kerriea-star <ojared511@gmail.com>
2024-08-28 07:35:30 +0300

    modulus operator

--- arithmetic_operators.c
+++ arithmetic_operators.c
@@ -7,14 +7,13 @@ int main(){
     // + (addition)
     // - (subtration)
     // * (multiplication)
-    // / (division)
-    // % (modulus)
     // ++ (increment)
     // -- (decrement)
 
     int x = 5;
     int y = 2;
 
+    // / (division)
     /*
     if you are dividing two numbers and your divisor is an integer, 
     then cast it as a float.
@@ -22,6 +21,14 @@ int main(){
     */ 
     float z = x / (float) y; 
 
-    printf("%d", z);
+    // % (modulus)
+    /*
+    It gives you the remainder of a division.
+    You can also use it to find out if a number is even or odd.
+    */
+    int a = x % y;
+
+    // printf("%d", z);
+    printf("%d", a);
     return 0;
 }
