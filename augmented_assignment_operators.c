86f948fcaabbc39a013a9cd906876ce728fce229
Kerriea-star <ojared511@gmail.com>
2024-08-28 07:51:55 +0300

    augmented assignment operators

---
+++ augmented_assignment_operators.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    /*
+    augmented assignment operators = used to replace a statement where an operator
+                                     takes a variable as one of its arguments
+                                     and then assigns the result back to the same variable
+                                     x = x + 1
+                                     x += 1
+    */
+   int x = 10;
+   // x =  x + 2
+   x += 2;
+   // x = x -3
+   x -= 3;
+   // x = x * 2
+   x *= 2;
+   // x = x / 2
+   x /= 2;
+   // x = x % 2
+   x %= 2;
+
+   printf("%d", x);
+   return 0;
+}
