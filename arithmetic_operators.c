d9a7a5efe50fe604fbdbdd33098f09a2a8808c7e
Kerriea-star <ojared511@gmail.com>
2024-08-28 07:23:26 +0300

    arithmetic operators

---
+++ arithmetic_operators.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+    // arithmetic operators
+
+    // + (addition)
+    // - (subtration)
+    // * (multiplication)
+    // / (division)
+    // % (modulus)
+    // ++ (increment)
+    // -- (decrement)
+
+    int x = 5;
+    int y = 2;
+
+    int z = x + y;
+
+    printf("%d", z);
+    return 0;
+}
