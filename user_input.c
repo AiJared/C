65a6236bf7c377a9ec8ccf71ce8022e789bc8840
Kerriea-star <ojared511@gmail.com>
2024-08-29 17:27:55 +0300

    string formating to remove new line tag from fgets

--- user_input.c
+++ user_input.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(){
 
@@ -10,6 +11,13 @@ int main(){
 
     // If your array of characters has white spaces then use fgets() as shown below
     fgets(name, 25, stdin);
+    /*
+    fgets comes with \n which means that it will break the line into two and
+    continue it on a new line. If you want to get rid of that \n then you'll first
+    #include <string.h> at the top which basically allows you to work with strings then
+    do the following:
+    */
+    name[strlen(name)-1] = '\0';
 
     printf("\nHow old are you?");
     scanf("%d", &age);
