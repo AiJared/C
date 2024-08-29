8b201424821082d852b8639bc30f2c2b55a624a0
Kerriea-star <ojared511@gmail.com>
2024-08-29 17:17:51 +0300

    fgets enables you to create an array of characters with whitespaces

--- user_input.c
+++ user_input.c
@@ -2,11 +2,14 @@
 
 int main(){
 
-    char name[25];
+    char name[25]; // bytes
     int age;
 
     printf("\nWhat's your name?");
-    scanf("%s", &name);
+    scanf("%s", &name); // does not support white spaces
+
+    // If your array of characters has white spaces then use fgets() as shown below
+    fgets(name, 25, stdin);
 
     printf("\nHow old are you?");
     scanf("%d", &age);
